29c87631d744f66b0921f139bd97cc1bad318afc
Filipe Vasconcelos <filipe.vasconcelos@esme.fr>
2025-12-13 00:05:45 +0100

    exemples bode plot,tikz

--- src/ftransfert/main_bode_plot.py
+++ src/ftransfert/main_bode_plot.py
@@ -10,12 +10,11 @@ if __name__ == "__main__" :
         print(repr(H0))
         print(str(H0))
         print(H0.eval(1,1))
-        bode(H0,y1lim=(-35,10),y2lim=(-90,0),n=1024,color="tab:green")
+        bodeplot(H0,y1lim=(-35,10),y2lim=(-90,0),n=1024,color="tab:green")
     if True :
         gain=100
         poles=[(-0.01,0),(-0.1,0),(-100,0)]
         zeros=[(-1,0),(-1,0)]
         H2=Ftransfert(zeros=zeros,poles=poles,gain=gain,name="H_2")
         print(repr(H2))
-        #bodeplot(H2,fmax=1e5,xlim=(1e-4,1e5),y1lim=(-80,80),y2lim=(-180,0),n=1024,color="tab:green")
-        bodetikz(H2)
+        bodeplot(H2,fmax=1e5,xlim=(1e-4,1e5),y1lim=(-80,80),y2lim=(-180,0),n=1024,color="tab:green")
--- src/ftransfert/main_bode_tikz.py
+++ src/ftransfert/main_bode_tikz.py
@@ -2,11 +2,19 @@ from common.Ftransfert import Ftransfert
 from bode.tikz import bode as bodetikz
 
 if __name__ == "__main__" :
-    with open("tmp.tex","w") as f:
-        if True :
-            gain=100
-            poles=[(-0.01,0),(-0.1,0),(-100,0)]
-            zeros=[(-1,0),(-1,0)]
-            H2=Ftransfert(zeros=zeros,poles=poles,gain=gain,name="H_2")
-            print(H2)
-            print(bodetikz(H2),file=f)
+    if True :
+        gain=100
+        poles=[(-0.01,0),(-0.1,0),(-100,0)]
+        zeros=[(-1,0),(-1,0)]
+        H2=Ftransfert(zeros=zeros,poles=poles,gain=gain,name="H_2")
+        H2.info()
+        print(f"str : {H2}")
+        with open("tmp1.tex","w") as f:
+            print(bodetikz(H2,wlim=(1e-4,1e4)),file=f)
+        num=[1,2,1]  #p**2+2p+1 
+        den=[1,100.11,11.001,0.1]  #p**3+100.11*p**2+11.001*p+0.1
+        H2=Ftransfert(num=num,den=den,gain=gain,name="H_2")
+        print(f"str : {H2}")
+        H2.info()
+        with open("tmp2.tex","w") as f:
+            print(bodetikz(H2,wlim=(1e-4,1e4)),file=f)
