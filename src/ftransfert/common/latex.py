e46f61b6a6d3354ca623f13ef2aeffe6c427dac8
Filipe Vasconcelos <filipe.vasconcelos@esme.fr>
2025-12-13 00:03:07 +0100

    la fonction tablatex est maintenant dans la classe principale

--- src/ftransfert/common/latex.py
+++ src/ftransfert/common/latex.py
@@ -36,26 +36,3 @@ def begin(env,options=""):
 def end(env):
     return "\\end{"f"{env}""}"
 # ------------------------------------------------------------------------------
-def tabLaTeX(self,**kwargs):
-   # Recast levels to new class
-   class nf(float):
-       def __repr__(self):
-           return f'{self:.2f}'
-   ws=kwargs.get('ws',None)
-   winput=isinstance(ws,np.ndarray)
-   wlim=kwargs.get('wlim', (1e-2,1e2))
-   n=kwargs.get('n',11)
-   # array of pulsations
-   if not winput :
-       ws=1j*np.logspace(np.log10(wlim[0]),np.log10(wlim[1]),n)
-   response=self.harm_response(ws,self.gain)
-   print("\\begin{center}")
-   print("\\begin{tabular}{ccc}")
-   print("\\hline")
-   print("$\omega$ (\si{\\radian\per\second}) & Gain (\si{\decibel}) & Phase (\si{\degree})\\\\")
-   print("\\hline")
-   for w,m,p in zip(ws,response[2],response[3]):
-       print(str(nf(abs(w)))+" & "+str(nf(nat2dB(m)))+" & "+str(nf(rad2deg(p)))+"\\\\")
-   print("\\hline")
-   print("\end{tabular}")
-   print("\\end{center}")
