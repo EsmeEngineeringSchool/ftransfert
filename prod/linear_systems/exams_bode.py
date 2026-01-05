33ec7a0e42d534cd30a5ceda1b0d6abc14e1e078
Filipe Vasconcelos <filipe.manuel.vasconcelos@gmail.com>
2026-01-05 17:56:31 +0100

    working version

--- prod/linear_systems/exams_bode.py
+++ prod/linear_systems/exams_bode.py
@@ -3,32 +3,26 @@ from ftransfert.bode.tikz import bode as bodetikz
 from ftransfert.bode.plot import bode as bodeplot
 
 if __name__ == "__main__" :
-    if True :
-        gain=4
+    if False :
+        gain=16
         num=[1,1]
-        den=[0.05,0.6,1,0]
+        den=[0.1,1,0,0]
         H=Ftransfert(num=num,den=den,gain=gain,name="H")
         H.info()
-        print(f"str : {H}")
-        print(f"eval jw : {H.eval(1j)}")
         filename="exercice1_1.tex"
-        print(f"writing {filename}")
-        with open(filename,"w") as f:
-            print(bodetikz(H,wlim=(1e-1,1e2),gain_axis=(-50,40,10),phase_axis=(-180,0,20)),file=f)
-        bodeplot(H,fmax=1e6,xlim=(1e-1,1e2),y1lim=(-80,40),y2lim=(-180,0))
+        bodetikz(H,filename,xlim=(1e-1,1e2),y1lim=(-40,40),y2lim=(-180,-90))
+        bodeplot(H,fmax=1e6,xlim=(1e-1,1e2),y1lim=(-40,40),y2lim=(-180,-90))
     if True :
-        gain=4
+        gain=40
         zeros=[(-1,0)]
-        poles=[(0,0),(-2,0),(-10,0)]
+        poles=[(0,0),(0,0),(-10,0)]
         H=Ftransfert(zeros=zeros,poles=poles,gain=gain,name="H")
         H.info()
         print(f"str : {H}")
         print(f"eval jw : {H.eval(1j)}")
-        filename="exercice1_2.tex"
-        print(f"writing {filename}")
-        with open(filename,"w") as f:
-            print(bodetikz(H,wlim=(1e-1,1e2),gain_axis=(-50,40,10),phase_axis=(-180,0,20)),file=f)
-        bodeplot(H,fmax=1e6,xlim=(1e-1,1e2),y1lim=(-80,40),y2lim=(-180,0))
+        filename="exercice1_1.tex"
+        bodetikz(H,filename,xlim=(1e-1,1e2),y1lim=(-40,40),y2lim=(-180,-90))
+        bodeplot(H,fmax=1e6,xlim=(1e-1,1e2),y1lim=(-40,40),y2lim=(-180,-90))
     if False :
         gain=1
         num=[4,4]
